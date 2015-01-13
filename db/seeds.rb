bc44935be37e7e337b09bf88469770e38827889e
Orhan Can Ceylan <can.ceylan@derivalabs.com>
2015-01-13 11:15:45 +0200

    Generated database seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Client.create({name: "Zara", branch:"Istinye Park"})
+Client.create({name: "Zara", branch:"Kanyon"})
+Client.create({name: "Zara", branch:"Marmaris Marina"})
+
+Location.create({client_id: 1, region: "Outside South", minX: -0.5, maxX: 190, minY: -0.5, maxY: 20})
+Location.create({client_id: 1, region: "Outside West", minX: -0.5, maxX: 25, minY: -0.5, maxY: 105})
+Location.create({client_id: 1, region: "Women", minX: 25, maxX: 120, minY: 60, maxY: 105})
+Location.create({client_id: 1, region: "Men", minX: 120, maxX: 190, minY: 20, maxY: 60})
+Location.create({client_id: 1, region: "All", minX: 25, maxX: 120, minY: 20, maxY: 60})
+Location.create({client_id: 1, region: "Checkout", minX: 120, maxX: 190, minY: 60, maxY: 105})
+
+
