3e1ba8cf7cfc901ad3febf74ec345bf9c785e8b9
Orhan Can Ceylan <can.ceylan@derivalabs.com>
2015-01-13 14:25:02 +0200

    Generate logs controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,34 @@
 class LogsController < ApplicationController
+
+	def add_log
+
+		resource = RestClient::Resource.new(
+											"https://learning:learning@64.103.26.61/api/contextaware/v1/location/history/clients/00:00:2a:01:00:0f.json",
+											:timeout => -1,
+											:open_timeout => -1)
+		response = resource.get  :params => {sortBy: "lastLocatedTime:desc", pageSize: 500, page: 2}
+
+		totalPages = JSON.parse(response)["Locations"]["totalPages"]
+		currentPage = JSON.parse(response)["Locations"]["currentPage"]
+
+		puts response
+		visitor_list = JSON.parse(response)["Locations"]["entries"]
+		#variables
+		visitor_list.each do |v|
+			Log.create({mac_id: v["macAddress"], 
+						reason: v["historyLogReason"],
+						hierarchy: v["MapInfo"]["mapHierarchyString"],
+						mapX: v["MapCoordinate"]["x"],
+						mapY: v["MapCoordinate"]["y"],
+						firstTime: v["Statistics"]["firstLocatedTime"],
+						lastTime: v["Statistics"]["lastLocatedTime"],
+						serverTime: v["Statistics"]["currentServerTime"],
+						location: Location.get_location(v["MapCoordinate"])
+						})
+		end
+	
+
+	end
+
+
 end
