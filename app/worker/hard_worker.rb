d91a8d6bfc6675bb3a1b74ca1ee35883d67d8c3f
Orhan Can Ceylan <can.ceylan@derivalabs.com>
2015-01-13 14:17:43 +0200

    Generated controller

---
+++ app/worker/hard_worker.rb
@@ -0,0 +1,9 @@
+class HardWorker
+
+	include Sidekiq::Worker
+
+	def perform(name, count)
+		puts 'Doing hard work'
+	end
+
+end
---
+++ test/controllers/session_logs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SessionLogsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
