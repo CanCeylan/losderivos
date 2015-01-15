2959379f408788d5759821bd994ada6111a3bd16
AhmetTanakol <ahmettanakol@gmail.com>
2015-01-15 21:02:40 +0200

    final version of metric worker

--- app/worker/metrics_worker.rb
+++ app/worker/metrics_worker.rb
@@ -18,13 +18,13 @@ class MetricsWorker
 			repeatCustomers = Log.select("COUNT(DISTINCT(macID)) as repeatCustomers").where("client_id = ? and date(firstLocatedTime) < date(lastLocatedTime)",client_id)
 			averageTime = SessionLog.select("SUM(duration)/COUNT(macID) as averageTime").where("client_id = ? and date(pointer) = ? and isClosed = 1",client_id,logDate) 
 			bounce = SessionLog.select("COUNT(macID) as bounce").where("client_id = ? and date(pointer) = ? and duration < 60 and isClosed = 1",client_id,logDate) 
-			weeklyRetention = Retention.select("SUM(w1)/COUNT(macID) as weeklyRetention").where("date(firstLocatedTime) = date_sub(date(?), INTERVAL 1 week)", logDate)
-			monthlyRetention= Retention.select("SUM(CASE WHEN date(firstLocatedTime) = date_sub(date(?) , INTERVAL 1 month)
-				and ((w1= 1) or (w2=1) or (w3=1) or (w4=1)) THEN 1 ELSE 0 END) /
-				SUM(CASE WHEN DATE(firstLocatedTime) = date_sub(DATE(?) , INTERVAL 1 MONTH) THEN 1 ELSE 0 END as monthlyRetention)",logDate,logDate)
-			biMonthlyRetention = Retention.select("SUM(CASE WHEN date(firstLocatedTime) = date_sub(date(?) , INTERVAL 2 month)
-				and ((w1= 1) or (w2=1) or (w3=1) or (w4=1)) THEN 1 ELSE 0 END) /
-				SUM(CASE WHEN DATE(firstLocatedTime) = date_sub(DATE(?) , INTERVAL 2 MONTH) THEN 1 ELSE 0 END as biMonthlyRetention)",logDate,logDate)
+			weeklyRetention = Retention.select("SUM(w1)/COUNT(macID) as weeklyRetention").where("client_id = #{client_id} and date(firstLocatedTime) = date_sub('#{logDate}', INTERVAL 1 week)")
+			monthlyRetention= Retention.select("SUM(CASE WHEN date(firstLocatedTime) = date_sub('#{logDate}' , INTERVAL 1 month)
+				and ((w1= 1) or (w2=1) or (w3=1) or (w4=1)) and client_id = #{client_id} THEN 1 ELSE 0 END) /
+				SUM(CASE WHEN client_id = #{client_id} and date(firstLocatedTime) = date_sub('#{logDate}' , INTERVAL 1 MONTH) THEN 1 ELSE 0 END) as monthlyRetention")
+			biMonthlyRetention = Retention.select("SUM(CASE WHEN date(firstLocatedTime) = date_sub('#{logDate}' , INTERVAL 2 month)
+				and ((w1= 1) or (w2=1) or (w3=1) or (w4=1)) and client_id = #{client_id} THEN 1 ELSE 0 END) /
+				SUM(CASE WHEN client_id = #{client_id} and date(firstLocatedTime) = date_sub('#{logDate}' , INTERVAL 2 MONTH) THEN 1 ELSE 0 END) as biMonthlyRetention")
 
 
 			metric = Metric.create({ logDate: logDate,
