31379a664936f168b91fb1bb9ef38e773175cf57
AhmetTanakol <ahmettanakol@gmail.com>
2015-01-15 14:15:46 +0200

    metrics queries done except retention

--- app/worker/metrics_worker.rb
+++ app/worker/metrics_worker.rb
@@ -3,7 +3,7 @@ class MetricsWorker
 	include Sidekiq::Worker
 
 	def perform()
-		puts "Basliyoruum #{name} reaally hard work #{count}"
+		puts "Basliyoruum"
 
 
 		client_list = Client.select(:id).distinct
@@ -11,27 +11,28 @@ class MetricsWorker
 		client_list.each do |c|
 
 			logDate= Date.today
-			client_id=c.id
-			potential= Location.joins(:logs).select("COUNT(DISTINCT(macID))").where("client_id = ? and date(lastLocatedTime) = ? and isOutside = 1",client_id,logDate)
-			conversion= Location.joins(:logs).select("COUNT(DISTINCT(macID))").where("client_id = ? and date(lastLocatedTime) = ? and isOutside = false",client_id,logDate)
-			newCustomers = Log.select("COUNT(DISTINCT(macID))").where("client_id = ? and date(firstLocatedTime) >= date(lastLocatedTime)",client_id)
-			repeatCustomers = Log.select("COUNT(DISTINCT(macID))").where("client_id = ? and date(firstLocatedTime) < date(lastLocatedTime)",client_id)
-			averageTime = SessionLog.select("SUM(duration)/COUNT(macID)").where("client_id = ? and date(logTime) = ? and isClosed = 1",client_id,logDate) 
-			bounce = SessionLog.select("COUNT(DISTINCT(macID))").where("client_id = ? and date(logTime) = ? and duration < 60 and isClosed = 1",client_id,logDate) 
+			client_id=c["id"]
+			potential= Location.joins(:logs).select("COUNT(DISTINCT(macID)) as potential").where("logs.client_id = ? and date(lastLocatedTime) = ? and isOutside = 1",client_id,logDate)
+			conversion= Location.joins(:logs).select("COUNT(DISTINCT(macID)) as conversion").where("logs.client_id = ? and date(lastLocatedTime) = ? and isOutside = 0",client_id,logDate)
+			newCustomers = Log.select("COUNT(DISTINCT(macID)) as newCustomers").where("client_id = ? and date(firstLocatedTime) >= date(lastLocatedTime)",client_id)
+			repeatCustomers = Log.select("COUNT(DISTINCT(macID)) as repeatCustomers").where("client_id = ? and date(firstLocatedTime) < date(lastLocatedTime)",client_id)
+			averageTime = SessionLog.select("SUM(duration)/COUNT(macID) as averageTime").where("client_id = ? and date(pointer) = ? and isClosed = 1",client_id,logDate) 
+			bounce = SessionLog.select("COUNT(macID) as bounce").where("client_id = ? and date(pointer) = ? and duration < 60 and isClosed = 1",client_id,logDate) 
 
-
-			Metric.create({ logDate: logDate,
+			metric = Metric.create({ logDate: logDate,
 				client_id: client_id,
-				potential: potential,
-				conversion: conversion,
-				newCustomers: newCustomers,
-				repeatCustomers: repeatCustomers,
-				averageTime: averageTime,
-				bounce: bounce,
+				potential: potential.first["potential"],
+				conversion: conversion.first["conversion"],
+				newCustomers: newCustomers.first["newCustomers"],
+				repeatCustomers: repeatCustomers.first["repeatCustomers"],
+				averageTime: averageTime.first["averageTime"],
+				bounce: bounce.first["bounce"],
 				weeklyRetention: 1.1, #weeklyRetention,
 				monthlyRetention: 1.1, #monthlyRetention,
 				biMonthlyRetention: 1.1 #biMonthlyRetention
 				})
+
+			metric.save
 
 		end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :metrics
-
-  get 'update/metrics' => 'metrics#writeMetrics'
   # You can have the root of your site routed with "root"
   #  root 'session_logs#ocan'
 
